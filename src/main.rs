a9304bfeb9ecc851ce10c02c74562b4d6d93edfc
Adrian Neumann <adrian_neumann@gmx.de>
2017-10-21 09:35:06 +0200

    copy paste path walk sample from docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+use std::fs::{self, DirEntry};
+use std::path::Path;
+
+// one possible implementation of walking a directory only visiting files
+fn visit_dirs(dir: &Path) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dirs(&path)?;
+            } else {
+                println!("{:?}", path);
+            }
+        }
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    visit_dirs(Path::new("."));
 }
