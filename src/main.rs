f68bca2ace2f757322f1033929d9fbeaac358228
Adrian Neumann <adrian_neumann@gmx.de>
2017-10-28 09:45:41 +0200

    input with backspace

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+extern crate termion;
+
 use std::io;
 use std::fs::{self, DirEntry};
 use std::path::Path;
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use termion::cursor;
+use std::io::{Write, stdout, stdin};
 
 // one possible implementation of walking a directory only visiting files
 fn visit_dirs(dir: &Path) -> io::Result<()> {
@@ -19,5 +26,17 @@ fn visit_dirs(dir: &Path) -> io::Result<()> {
 }
 
 fn main() {
-    visit_dirs(Path::new("."));
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    for c in stdin.keys() {
+        match c.unwrap() {
+            Key::Ctrl('q') => break,
+            Key::Backspace => {
+                print!("{} {}", cursor::Left(1), cursor::Left(1));
+            },
+            Key::Char(x) => print!("{}",x),
+            _ => {},
+        }
+        stdout.flush();
+    }
 }
