c3d67eeae0b8dd9cf8c256348671de0d7d41122e
Adrian Neumann <adrian_neumann@gmx.de>
2017-11-08 06:18:17 +0100

    dynamic programming scoring

--- src/similarity/mod.rs
+++ src/similarity/mod.rs
@@ -1,11 +1,70 @@
 use std::cmp::min;
 
 pub fn score(pattern: &str, text: &str) -> isize {
-    score_rec(pattern.as_bytes(), 0, text.as_bytes(), 0)
+    //score_rec(pattern.as_bytes(), 0, text.as_bytes(), 0)
+    score_iter(pattern.as_bytes(), text.as_bytes())
 }
 
 const TEXT_SKIP_PENALTY: isize = 1;
 const PATTERN_SKIP_PENALTY: isize = 10;
+
+fn score_iter(pattern: &[u8], text: &[u8]) -> isize {
+    let mut scores = vec![0; (1 + pattern.len()) * (1 + text.len())];
+    let index = |p, t| t * (pattern.len()+1) + p;
+    for p in 0..pattern.len() + 1 {
+        scores[index(p, 0)] = p as isize * PATTERN_SKIP_PENALTY;
+        //println!("init {} 0 {}", p, scores[index(p,0)]);
+    }
+    for t in 0..text.len() + 1 {
+        scores[index(0, t)] = t as isize * TEXT_SKIP_PENALTY;
+        //println!("init 0 {} {}", t, scores[index(0,t)]);
+    }
+
+    for p in 1..pattern.len() + 1 {
+        for t in 1..text.len() + 1 {
+            let m = min(
+                scores[index(p - 1, t)] + PATTERN_SKIP_PENALTY,
+                scores[index(p, t - 1)] + TEXT_SKIP_PENALTY,
+            );
+            if pattern[p - 1] == text[t - 1] {
+                scores[index(p, t)] = min(m, scores[index(p - 1, t - 1)]);
+//                if scores[index(p - 1, t - 1)] < m {
+//                    println! (
+//                        "cost {} {} {}, match {} {}",
+//                        p,
+//                        t,
+//                        scores[index(p, t)],
+//                        pattern[p - 1] as char,
+//                        text[t - 1] as char
+//                    );
+//                }
+            } else {
+                scores[index(p, t)] = m;
+ //               if scores[index(p - 1, t)] + PATTERN_SKIP_PENALTY <
+ //                   scores[index(p, t - 1)] + TEXT_SKIP_PENALTY
+ //               {
+ //                   println!(
+ //                       "cost {} {} {}, skip pattern {}",
+ //                       p,
+ //                       t,
+ //                       m,
+ //                       pattern[p - 1] as char
+ //                   );
+ //               } else {
+ //                   println!(
+ //                       "cost {} {} {}, skip text {}",
+ //                       p,
+ //                       t,
+ //                       m,
+ //                       text[t - 1] as char
+ //                   );
+ //               }
+            }
+        }
+    }
+
+    scores[index(pattern.len(), text.len())]
+}
 
 fn score_rec(pattern: &[u8], pi: usize, text: &[u8], pt: usize) -> isize {
     if pi >= pattern.len() {
@@ -26,27 +85,27 @@ fn score_rec(pattern: &[u8], pi: usize, text: &[u8], pt: usize) -> isize {
 
 #[test]
 fn score_equality() {
-    let s = score("aoeu","aoeu");
+    let s = score("aoeu", "aoeu");
     println!("score aoeu aoeu: {}", s);
-    assert!(s==0);
+    assert!(s == 0);
 }
 
 #[test]
 fn score_substring() {
     {
-    let s = score("aoeu","xyzaoeuLMN");
-    println!("score aoeu xyzaoeuLMN: {}", s);
-    assert!(s == 6*TEXT_SKIP_PENALTY);
+        let s = score("aoeu", "xyzaoeuLMN");
+        println!("score aoeu xyzaoeuLMN: {}", s);
+        assert!(s == 6 * TEXT_SKIP_PENALTY);
     }
 
-    let s = score("xyzaoeuLMN","aoeu");
-    println!("score aoeu xyzaoeuLMN: {}", s);
-    assert!(s == 6*PATTERN_SKIP_PENALTY);
+    let s = score("xyzaoeuLMN", "aoeu");
+    println!("score xyzaoeuLMN aoeu: {}", s);
+    assert!(s == 6 * PATTERN_SKIP_PENALTY);
 }
 
 #[test]
 fn score_subsequence() {
-    let s = score("aoeu","xayozeLu");
+    let s = score("aoeu", "xayozeLu");
     println!("score aoeu xayozeLu: {}", s);
-    assert!(s == 4*TEXT_SKIP_PENALTY);
+    assert!(s == 4 * TEXT_SKIP_PENALTY);
 }
