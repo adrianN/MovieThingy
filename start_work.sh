625924499a092f4f67a8ac295b96bab33331b644
Adrian Neumann <adrian_neumann@gmx.de>
2017-11-15 07:09:17 +0100

    start cargo check

--- start_work.sh
+++ start_work.sh
@@ -2,7 +2,7 @@
 
 tmux new-session -s MovieThingy \; \
  split-window -h -p '65' \; \
- send-keys -t 0 'cd ~/MovieThingy' Enter \; \
+ send-keys -t 0 'cd ~/MovieThingy && cargo check' Enter \; \
  send-keys -t 1 'cd ~/MovieThingy; vim' Enter \;
 
 
